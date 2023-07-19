abb2b06d3b4271b86bd26be52dd87c2bfe0454f3
Kevin Pham <phamtkevin@gmail.com>
2023-07-18 19:54:40 -0700

    Mark face with a rectangle

--- facial_recognition.py
+++ facial_recognition.py
@@ -10,6 +10,32 @@ test_picture = fr.load_image_file('PictureB.jpg')
 control_picture = cv2.cvtColor(control_picture, cv2.COLOR_BGR2RGB)
 test_picture = cv2.cvtColor(test_picture, cv2.COLOR_BGR2RGB)
 
+# locate control face
+face_A_location = fr.face_locations(control_picture)[0]
+coded_face_A = fr.face_encodings(control_picture)[0]
+
+# locate test face
+face_B_location = fr.face_locations(test_picture)[0]
+coded_face_B = fr.face_encodings(test_picture)[0]
+
+# frame the control face
+cv2.rectangle(control_picture,
+              (face_A_location[3], face_A_location[0]),
+              (face_A_location[1], face_A_location[2]),
+              (0, 255, 0),
+              2)
+
+# frame the test face
+cv2.rectangle(test_picture,
+              (face_B_location[3], face_B_location[0]),
+              (face_B_location[1], face_B_location[2]),
+              (0, 255, 0),
+              2)
+
+# perform comparison
+result = fr.compare_faces([coded_face_A], coded_face_B)
+print(result)
+
 # display images
 cv2.imshow('My Control Picture', control_picture)
 cv2.imshow('My Test Picture', test_picture)
